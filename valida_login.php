2b33a07c8a0beef9426231ffdfdde0ae2177d926
Diego Prates <prates.diego@gmail.com>
2025-08-26 16:23:21 -0300

    feat: protegendo páginas com Session

--- valida_login.php
+++ valida_login.php
@@ -1,3 +1,23 @@
 <?php
-    $_POST['email'];
-    $_POST['senha'];
+    session_start();
+    //Usuários do Sistema
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+    //Variável que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
+
+    foreach($usuarios_app as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+        }
+    }
+    if($usuario_autenticado){
+        echo 'Usuário Autenticado';
+        $_SESSION['autenticado'] = 'SIM';
+    }else {
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: index.php?login=erro');
+        
+    }
